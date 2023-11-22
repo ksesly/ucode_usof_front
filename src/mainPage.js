e63a96753ff5ee2feb0a0ea336dc8e45237de18e
ksesly <ks.you.04@gmail.com>
2023-11-23 00:46:02 +0200

    start of the project

---
+++ src/mainPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/mainPage.scss';
+import './style/global.scss';
+import Post from '.';
+
+
+function mainPage() {
+    return (
+        <div className='mainPage'></div>
+    )
+}
+
+
+
+
