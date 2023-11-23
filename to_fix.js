2ac3edbf6e1a906b6f529f7b79a5d5ffe3594023
ksesly <ks.you.04@gmail.com>
2023-11-23 17:37:27 +0200

    taska added

---
+++ to_fix.js
@@ -0,0 +1,22 @@
+//GLOBAL
+
+// 1 make the first page the mainPage 
+
+
+//AUTH
+
+// 1 to add the password resrt link
+// 2 style register
+// 3 style login
+// 4 page with reset password [email]
+// 5 reset password style
+// 6 page with reset password [password]
+// 7 reset password style
+// 8 handle all errors login
+// 9 handle all errors register
+
+//MAIN PAGE
+
+// 1 view all posts 
+// 2 navbar
+// ...
