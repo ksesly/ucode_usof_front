dbb9cf781baa5f859ada67576726d10941db840c
ksesly <ks.you.04@gmail.com>
2023-11-25 00:08:10 +0200

    not good try of avatar

--- src/pages/globalComponents/avatar.jsx
+++ src/pages/globalComponents/avatar.jsx
@@ -1,36 +1,94 @@
+// import React, { useState, useEffect } from 'react';
+// import '../../style/header.scss';
+// import axios from 'axios';
+// import Cookies from 'js-cookie';
+
+// const Avatar = () => {
+// 	const [profilePic, setProfilePic] = useState({});
+// 	useEffect(() => {
+// 		const fetchUserData = async () => {
+// 			try {
+// 				const token = Cookies.get('token');
+
+// 				if (token) {
+// 					console.log('Token exists:', token);
+// 					const response = await axios.get(
+// 						'http://127.0.0.1:3050/api/users/currentUser',
+// 						{ responseType: 'blob' },
+// 						{
+// 							headers: {
+// 								Authorization: `Bearer%20${token}`,
+// 							},
+// 						}
+// 					);
+
+// 					console.log(response.data);
+// 					let imgUrl = URL.createObjectURL(response.data);
+// 					setProfilePic(imgUrl);
+// 					console.log(imgUrl);
+// 				} else {
+// 					console.log('Token does not exist');
+// 				}
+// 			} catch (error) {
+// 				console.error('Error fetching user data:', error);
+// 			}
+// 		};
+
+// 		fetchUserData();
+// 	}, []);
+
+// 	return (
+// 		<div className="header">
+// 			<img
+// 				src={profilePic.profilePicture}
+// 				style={{
+// 					width: '50px',
+// 					height: '50px',
+// 					// borderRadius: '50%',
+// 				}}
+// 			/>
+// 		</div>
+// 	);
+// };
+
+// export default Avatar;
+
 import React, { useState, useEffect } from 'react';
-import '../../style/header.scss';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const Avatar = () => {
-	const [profilePic, setProfilePic] = useState({});
+const Header = () => {
+	const [userData, setUserData] = useState({});
+
 	useEffect(() => {
 		const fetchUserData = async () => {
 			try {
 				const token = Cookies.get('token');
 
 				if (token) {
-					console.log('Token exists:', token);
+					
 					const response = await axios.get(
 						'http://127.0.0.1:3050/api/users/currentUser',
-						{ responseType: 'blob' },
 						{
 							headers: {
 								Authorization: `Bearer%20${token}`,
 							},
 						}
 					);
+                    
+		
+					
+					
 
-					console.log(response.data);
-					let imgUrl = URL.createObjectURL(response.data);
-					setProfilePic(imgUrl);
-					console.log(imgUrl);
+					setUserData((prevUserData) => ({
+						...prevUserData,
+						...userData,
+					}));
 				} else {
-					console.log('Token does not exist');
+					// console.log('Token does not exist');
 				}
 			} catch (error) {
-				console.error('Error fetching user data:', error);
+				// console.error('Error fetching user data:', error);
 			}
 		};
 
@@ -38,17 +96,24 @@ const Avatar = () => {
 	}, []);
 
 	return (
-		<div className="header">
-			<img
-				src={profilePic.profilePicture}
-				style={{
-					width: '50px',
-					height: '50px',
-					// borderRadius: '50%',
-				}}
-			/>
+		<div className="header-field">
+			{userData.avatarUrl ? (
+				<img
+					src={userData.avatarUrl}
+					// alt="Avatar"
+					style={{
+						width: '50px',
+						height: '50px',
+						borderRadius: '50%',
+						display: 'block',
+					}}
+				/>
+			) : (
+				<span>No avatar available</span>
+			)}
+			
 		</div>
 	);
 };
 
-export default Avatar;
+export default Header;
